10203ee15eb8d85f93184fcf78de08754d9a0328
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2022-04-03 19:28:08 -0300

    Commit

--- metodosOrdenacao/arquivo/src/com/company/Main.java
+++ metodosOrdenacao/arquivo/src/com/company/Main.java
@@ -3,12 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-//        Arquivo arquivo = new Arquivo("trabalhoPo.bin");
-
         Principal p = new Principal();
         p.geraTabela();
-
-
-
     }
 }
